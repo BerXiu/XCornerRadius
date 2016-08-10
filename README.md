8de23ef6af2c59a5f452e18ab902614091053f17
Xiu <gogs@fake.local>
2016-08-10 11:54:19 +0800

    Update the README.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-#XCornerRadius
+<h1 align="center">CornerRadius</h1>
+<br>
+	<p align="center">
+		<img src="https://img.shields.io/badge/version-v1.0.0-blue.svg" />
+		<img src="https://img.shields.io/badge/language-OC-red.svg" />
+	</p>
+	<p align="center">
+		<img width=425 height=327 src="https://raw.githubusercontent.com/BerXiu/MyResource/master/XCornerRadius/SimulatorImage.png" />
+	</p>
+	<h5 align="center">Efficient rounded Settings, make your program fly</h5>
+</br>
+
+##CocoaPods
+```
+pod 'XCornerRadius', '~> 1.0.0'
+```
+
+##Usage
+
+#####Create Rounding Image
+```
+	UIImageView *roundingImage = [[UIImageView alloc]initWithRoundingRectImageView];
+	roundingImage.image = [UIImage imageNamed:@"testImage"];
+	[roundingImage setFrame:CGRectMake(centerX - 120, centerY - 120, 100, 100)];
+	[self.view addSubview:roundingImage];
+```
+
+#####set the coustom ConrnerRadius
+```
+    UIImageView *cornerRadius = [[UIImageView alloc]initCornerRadius:25 type: UIRectCornerTopLeft | UIRectCornerBottomRight];
+    cornerRadius.image = [UIImage imageNamed:@"testImage"];
+    [cornerRadius setFrame:CGRectMake(centerX - 120, centerY + 20, 100, 100)];
+    [self.view addSubview:cornerRadius];
+```
+
+#####set the borderWidth and borderColor
+```
+	[roundingImage x_setBorderWidth:5 borderColor:[UIColor orangeColor]];
+```
--- XCornerRadius/ViewController.m
+++ XCornerRadius/ViewController.m
@@ -22,25 +22,25 @@
     CGFloat centerY = (self.view.frame.size.height - 64) / 2;
     
     UIImageView *roundingImage = [[UIImageView alloc]initWithRoundingRectImageView];
-    roundingImage.image = [UIImage imageNamed:@"textImage"];
+    roundingImage.image = [UIImage imageNamed:@"testImage"];
     [roundingImage x_setBorderWidth:5 borderColor:[UIColor orangeColor]];
     [roundingImage setFrame:CGRectMake(centerX - 120, centerY - 120, 100, 100)];
     [self.view addSubview:roundingImage];
     
     UIImageView *roundingImage1 = [[UIImageView alloc]init];
     [roundingImage1 x_roundingRectImageView];
-    roundingImage1.image = [UIImage imageNamed:@"textImage"];
+    roundingImage1.image = [UIImage imageNamed:@"testImage"];
     [roundingImage1 setFrame:CGRectMake(centerX + 20, centerY - 120, 100, 100)];
     [self.view addSubview:roundingImage1];
     
     UIImageView *cornerRadius = [[UIImageView alloc]initCornerRadius:25 type: UIRectCornerTopLeft | UIRectCornerBottomRight];
-    cornerRadius.image = [UIImage imageNamed:@"textImage"];
-    [cornerRadius x_setBorderWidth:5 borderColor:[UIColor orangeColor]];
+    cornerRadius.image = [UIImage imageNamed:@"testImage"];
     [cornerRadius setFrame:CGRectMake(centerX - 120, centerY + 20, 100, 100)];
     [self.view addSubview:cornerRadius];
     
     UIImageView *cornerRadius1 = [[UIImageView alloc]initCornerRadius:25 type: UIRectCornerTopRight | UIRectCornerBottomLeft];
-    cornerRadius1.image = [UIImage imageNamed:@"textImage"];
+    cornerRadius1.image = [UIImage imageNamed:@"testImage"];
+    [cornerRadius1 x_setBorderWidth:5 borderColor:[UIColor orangeColor]];
     [cornerRadius1 setFrame:CGRectMake(centerX + 20, centerY + 20, 100, 100)];
     [self.view addSubview:cornerRadius1];
     
